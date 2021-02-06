f267f18c86eecd28327cf0305c5fbdebd1df461b
2bWant2b <tzh363231879@163.com>
2021-02-06 13:49:17 +0800

    Site updated: 2021-02-06 13:49:16

---
+++ js/background.js
@@ -0,0 +1,21 @@
+/*添加图片top*/
+var top_up = "<img id='upj' class='upj' style='max-width: 1000%; transform: translate(-70px,-70px);' src='https://cdn.jsdelivr.net/gh/lete114/CDN/Use/up.gif' title='回到顶部' >";
+/*添加到返回顶部按钮下*/
+document.getElementById("go-up").innerHTML += top_up;
+
+// 可爱的Title
+var OriginTitle = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function() {
+    if (document.hidden) {
+        $('[rel="icon"]').attr('href', "/img/favicon.ico");
+        document.title = '(つェ⊂) 我藏好了哦~~';
+        clearTimeout(titleTime);
+    } else {
+        $('[rel="icon"]').attr('href', "/img/favicon.ico");
+        document.title = '(*´∇｀*) 被你发现啦~~' + OriginTitle;
+        titleTime = setTimeout(function() {
+            document.title = OriginTitle;
+        }, 2000);
+    }
+});
