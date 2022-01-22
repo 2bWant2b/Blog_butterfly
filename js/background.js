d918417b519e167514d11416ea20e041e078d363
2bWant2b <tzh363231879@163.com>
2022-01-22 19:31:44 +0800

    Site updated: 2022-01-22 19:31:31

---
+++ js/background.js
@@ -0,0 +1,27 @@
+/*添加图片top*/
+/*var top_up = "<img id='upj' class='upj' style='max-width: 1000%; transform: translate(-70px,-70px);' src='https://cdn.jsdelivr.net/gh/lete114/CDN/Use/loading.gif' title='回到顶部' >";
+//https://cdn.jsdelivr.net/gh/lete114/CDN/Use/up.gif
+/*添加到返回顶部按钮下*/
+/*document.getElementById("go-up").innerHTML += top_up;*/
+
+// 可爱的Title
+var OriginTitle = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function() {
+    if (document.hidden) {
+        $('[rel="icon"]').attr('href', "/img/favicon.ico");
+        document.title = '(つェ⊂) 我藏好了哦~~';
+        clearTimeout(titleTime);
+    } else {
+        $('[rel="icon"]').attr('href', "/img/favicon.ico");
+        document.title = '(*´∇｀*) 被你发现啦~~' + OriginTitle;
+        titleTime = setTimeout(function() {
+            document.title = OriginTitle;
+        }, 2000);
+    }
+});
+//一图流
+var full_page = document.getElementsByClassName("full_page");
+if (full_page.length != 0) {
+  full_page[0].style.background = "transparent";
+}
---
+++ news/js/keys.js
@@ -0,0 +1,4 @@
+const newskey ={
+	id: 'tqirstejvtqmn7lo',
+	key: 'MnRlY2lDeE5ldU01Q2lvTWtVK293Zz09',
+}
